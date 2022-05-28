fe3049fb073e770b3c0f314c010d85421eaeb9fc
Divyanshu Khandelwal <67157526+Abhi-dr@users.noreply.github.com>
2022-05-28 18:17:12 +0530

    Update Electricity Unit Charges.c

--- If-Else/Electricity Unit Charges.c
+++ If-Else/Electricity Unit Charges.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
 
+int main()
+{
+    int units;
+    
+    printf("Enter the units you've used: ");
+    scanf("%d", &units);
+    
+    int total;
+    
+    if (units <= 50){
+        total = 0.5*units;
+    }
+    
+    else if (units <= 150){
+        total = 25 + ((units-50) * 0.75);
+    }
+    
+    else if(units <= 250)
+    {
+        total = 100 + ((units-150) * 1.20);
+    }
+    else
+    {
+        total = 220 + ((units-250) * 1.50);
+    }
+
+    int extra = total * 0.2;
+    total += extra;
+    
+    printf("Total charge: %d", total);
+    
+}
