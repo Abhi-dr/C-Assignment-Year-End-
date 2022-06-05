5fc1ab689181db890b3836c6b89e958f2b02757a
Divyanshu Khandelwal <67157526+Abhi-dr@users.noreply.github.com>
2022-06-05 12:43:37 +0530

    Update 1.c
    Write a C program to input temperature in Centigrade and convert to Fahrenheit

--- Assignment 3/1.c
+++ Assignment 3/1.c
@@ -1,1 +1,15 @@
-abi aa ra hu
+#include <stdio.h>
+
+int main()
+{
+    float a;
+    float celsius;
+    printf("Enter the Celsius: ");
+    scanf("%f", &celsius);
+    
+    float result = (1.8*celsius) + 32;
+    
+    printf("Fahrenheit = %0.2f", result);
+    
+    
+}
